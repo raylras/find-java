fdd4039d2d6d0a02eee13a34e9b87e5dc87768dc
raylras <61815515+raylras@users.noreply.github.com>
2023-08-04 02:20:38 +0800

    fix stderr

--- find-java.bat
+++ find-java.bat
@@ -1,13 +1,13 @@
 @echo off
 
 @REM Oracle
-for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft" /s ^| find " JavaHome "') do echo %%j
+for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft" /s 2^>^&1 ^| find " JavaHome "') do echo %%j 
 
 @REM Eclipse Adoptium
-for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Eclipse Adoptium" /s ^| find " Path "') do echo %%j
+for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Eclipse Adoptium" /s 2^>^&1 ^| find " Path "') do echo %%j
 
 @REM Microsoft
-for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\JDK" /s ^| find " Path "') do echo %%j
+for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\JDK" /s 2^>^&1 ^| find " Path "') do echo %%j
 
 @REM Azul Zulu
-for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Azul Systems\Zulu" /s ^| find " InstallationPath "') do echo %%j
+for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Azul Systems\Zulu" /s 2^>^&1 ^| find " InstallationPath "') do echo %%j
