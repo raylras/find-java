1e37e5ce8e1d8e7d3275f977840c5adb17eae4e2
raylras <raylras@outlook.com>
2023-08-04 21:54:29 +0800

    add 'IBM Semeru' and 'BellSoft Liberica'

--- find-java.bat
+++ find-java.bat
@@ -12,8 +12,14 @@ for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Eclipse Adop
 @REM Microsoft
 for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\JDK" /s 2^>nul ^| find " Path "') do @echo %%j >> %dup%
 
+@REM IBM Semeru
+for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Semeru" /s 2^>nul ^| find " Path "') do @echo %%j >> %dup%
+
 @REM Azul Zulu
 for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Azul Systems\Zulu" /s 2^>nul ^| find " InstallationPath "') do @echo %%j >> %dup%
+
+@REM BellSoft Liberica
+for /f "tokens=2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\BellSoft\Liberica" /s 2^>nul ^| find " InstallationPath "') do @echo %%j >> %dup%
 
 @REM remove duplicate rows
 sort /unique %dup% /o %dedup%
